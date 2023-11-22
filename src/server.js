bbc132f56835c99111fe41af20f4b4113888262b
LoRd123545 <kamilabbasi694@gmail.com>
2023-11-22 21:52:27 +0100

    basic mvc setup

--- src/server.js
+++ src/server.js
@@ -1,37 +1,51 @@
+/* external libs */
 const express = require('express');
-const ejsLayouts = require('express-ejs-layouts');
+const expressEjsLayouts = require('express-ejs-layouts');
 const session = require('express-session');
 const Keycloak = require('keycloak-connect');
 
-const mysql = require('./conifg/mysql-config');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const memoryStore = session.MemoryStore();
+
+/* ejs, express ejs layouts and static files config */
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(expressEjsLayouts);
+app.use(express.static(__dirname + '/static'));
+
+/* keycloak config */
 const keycloak = new Keycloak({
   store: memoryStore
 });
-
-app.set('view-engine', 'ejs');
-
-app.use(express.json());
-app.use(express.urlencoded({
-  extended: false
-}));
 app.use(keycloak.middleware({
   logout: '/logout'
 }));
 
-app.get('/', (req, res) => {
-  res.send('Hello');
-});
+/* config */
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: false
+}));
 
+/* routers */
+const homeRouter = require('./routes/home');
+const aboutRouter = require('./routes/about');
+const faqRouter = require('./routes/faq');
+const accountRouter = require('./routes/account');
+const tasksRouter = require('./routes/tasks');
+
+/* app routes */
+app.use('/', homeRouter);
+app.use('/faq', faqRouter);
+app.use('/about', aboutRouter);
+app.use('/tasks', tasksRouter);
+app.use('/account', accountRouter);
+
+// keycloak test - error
 app.get('/protected', keycloak.protect(), (req, res) => {
   res.send('Protected');
-});
-
-app.get('/dbtest', async (req, res) => {
-  const result = await mysql.query('SELECT * FROM test');
-  res.send(result[0]);
 });
 
 app.listen(PORT, () => {
