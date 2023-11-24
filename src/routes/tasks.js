90f5c3c2f006b7f0535e1bbc60157c997e6a6a60
LoRd123545 <kamilabbasi694@gmail.com>
2023-11-24 21:17:07 +0100

    added option to add task from a GUI using simple form. Task is added to database.

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,9 +1,33 @@
 const express = require('express');
 
 const router = express.Router();
+const taskModel = require('../models/taskModel');
 
-router.get('/', (req, res) => {
-    res.render('tasks');
-})
+router.get('/', async (req, res) => {
+  const tasks = await taskModel.getAll();
+
+  res.render('tasks', {
+    tasks: tasks
+  });
+  //res.send(tasks);
+});
+
+router.post('/', (req, res) => {
+  const {
+    name,
+    completion_date,
+    status
+  } = req.body;
+
+  const task = {
+    name: name,
+    completion_date: completion_date,
+    status: status
+  };
+
+  taskModel.add(task);
+
+  res.redirect('/tasks');
+});
 
 module.exports = router;
