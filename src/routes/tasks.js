4ce64b2b5cb82af3c82d65c061172ea65e6a4edc
LoRd123545 <kamilabbasi694@gmail.com>
2023-11-25 22:27:52 +0100

    added routes for all actions related to task

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,38 +1,53 @@
 const express = require('express');
+const { v4 : uuid } = require('uuid');
 
 const router = express.Router();
-const taskModel = require('../models/taskModel');
+const db = require('../models/taskModel');
 
 router.get('/', async (req, res) => {
-  const tasks = await taskModel.getAll();
+  const tasks = await db.getAllTasks();
 
   res.render('tasks', {
     tasks: tasks
   });
 });
 
-router.post('/add', (req, res) => {
+router.post('/add', async (req, res) => {
   const task = {
+    id: uuid(),
     name: req.body.name,
     completion_date: req.body.completion_date,
     status: req.body.status
   };
 
-  taskModel.add(task);
+  await db.addTask(task);
 
   res.redirect('/tasks');
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  const task = await db.getTask(req.params.id);
 
+  res.render('tasks/task', {
+    task: task
+  });
 });
 
-router.put('/:id/edit', (req, res) => {
+router.put('/:id/edit', async (req, res) => {
+  const updatedTask = {
+    name: req.body.name,
+    completion_date: req.body.completion_date,
+    status: req.body.status
+  };
 
+  await db.updateTask(req.params.id, updatedTask);
+
+  res.redirect('/tasks');
 });
 
-router.delete('/:id', (req, res) => {
-  taskModel.delete(req.params.id);
+router.delete('/:id', async (req, res) => {
+  await db.deleteTask(req.params.id);
+
   res.redirect('/tasks');
 });
 
