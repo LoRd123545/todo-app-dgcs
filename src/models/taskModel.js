dfb714655acb98c2f7f9099397c2d0e735639e2b
LoRd123545 <kamilabbasi694@gmail.com>
2023-11-25 22:24:14 +0100

    complete model for task

--- src/models/taskModel.js
+++ src/models/taskModel.js
@@ -1,57 +1,57 @@
 const mysql = require('../config/mysql-config');
 
-class TaskModel
-{
-  async add(task) {
-    const {
-      name,
-      completion_date,
-      status
-    } = task;
+async function getAllTasks() {
+  const result = await mysql.query(`
+    SELECT *
+    FROM tasks
+  `);
 
-    const task = await mysql.query(`
-      INSERT INTO tasks 
-      (name, completion_date, status)
-      VALUES(?, ?, ?)
-    `, [name, completion_date, status]);
-
-    return task[0][0];
-  }
-
-  async update(name) {
-    
-  }
-
-  async delete(name) {
-    const task = await mysql.query(`
-      DELETE
-      FROM tasks
-      WHERE name = ?
-    `, [ name ]);
-
-    return task[0][0];
-  }
-
-  async get(name) {
-    const task = await mysql.query(`
-      SELECT *
-      FROM tasks
-      WHERE name = ?
-    `, [ name ]);
-
-    return task[0][0];
-  }
-
-  async getAll() {
-    const tasks = await mysql.query(`
-      SELECT *
-      FROM tasks
-    `);
-
-    return tasks[0];
-  }
+  return result[0];
 }
 
-const taskModel = new TaskModel();
+async function getTask(id) {
+  const result = await mysql.query(`
+    SELECT *
+    FROM tasks
+    WHERE id = ?
+  `, [id]);
 
-module.exports = taskModel;
+  return result[0][0];
+}
+
+async function addTask(task) {
+  const result = await mysql.query(`
+    INSERT INTO tasks 
+    VALUES(?, ?, ?, ?)
+  `, [task.id, task.name, task.completion_date, task.status]);
+
+  return result[0][0];
+}
+
+async function updateTask(id, task) {
+  const result = await mysql.query(`
+    UPDATE tasks
+    SET name = ?, completion_date = ?, status = ?
+    WHERE id = ?
+  `, [task.name, task.completion_date, task.status, id]);
+
+  return result[0][0];
+}
+
+async function deleteTask(id) {
+  const result = await mysql.query(`
+    DELETE
+    FROM tasks
+    WHERE id = ?
+  `, [id]);
+
+  return result[0][0];
+}
+
+module.exports = {
+  getAllTasks,
+  getTask,
+  addTask,
+  updateTask,
+  deleteTask
+};
