e7a31d498bc02b758acee0bc3cdfacd0f4c3e3d0
LoRd123545 <kamilabbasi694@gmail.com>
2023-11-27 21:13:32 +0100

    added sql query to create table for tasks if it doesn't exist

--- src/config/mysql-config.js
+++ src/config/mysql-config.js
@@ -9,4 +9,13 @@ const pool = mysql.createPool({
   dateStrings: true
 }).promise();
 
+
+pool.query(`
+  CREATE TABLE
+  IF NOT EXISTS
+  tasks
+  (id varchar(36) primary key, name varchar(50), description varchar(500),
+  completion_date datetime, status varchar(20))
+`);
+
 module.exports = pool;
