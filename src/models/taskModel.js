82b9e0b7b30e69d31c6960749d2f046ffc5f3dae
LoRd123545 <kamilabbasi694@gmail.com>
2023-11-27 19:54:03 +0100

    added new column to taskModel

--- src/models/taskModel.js
+++ src/models/taskModel.js
@@ -1,51 +1,71 @@
 const mysql = require('../config/mysql-config');
 
 async function getAllTasks() {
-  const result = await mysql.query(`
-    SELECT *
-    FROM tasks
-  `);
+  try {
+    const result = await mysql.execute(`
+      SELECT *
+      FROM tasks
+    `);
 
-  return result[0];
+    return result[0];
+  } catch(err) {
+    console.error(err);
+  }
 }
 
 async function getTask(id) {
-  const result = await mysql.query(`
-    SELECT *
-    FROM tasks
-    WHERE id = ?
-  `, [id]);
-
-  return result[0][0];
+  try {
+    const result = await mysql.execute(`
+      SELECT *
+      FROM tasks
+      WHERE id = ?
+    `, [id]);
+  
+    return result[0][0];
+  } catch(err) {
+    console.error(err);
+  }
 }
 
 async function addTask(task) {
-  const result = await mysql.query(`
-    INSERT INTO tasks 
-    VALUES(?, ?, ?, ?)
-  `, [task.id, task.name, task.completion_date, task.status]);
-
-  return result[0][0];
+  try {
+    const result = await mysql.execute(`
+      INSERT INTO tasks 
+      VALUES(?, ?, ?, ?, ?)
+    `, [task.id, task.name, task.description, task.completion_date, task.status]);
+  
+    return result[0][0];
+  } catch(err) {
+    console.error(err);
+  }
 }
 
 async function updateTask(id, task) {
-  const result = await mysql.query(`
-    UPDATE tasks
-    SET name = ?, completion_date = ?, status = ?
-    WHERE id = ?
-  `, [task.name, task.completion_date, task.status, id]);
-
-  return result[0][0];
+  try {
+    const result = await mysql.execute(`
+      UPDATE tasks
+      SET name = ?, description = ?, completion_date = ?, status = ?
+      WHERE id = ?
+    `, [task.name, task.description, task.completion_date, task.status, id]);
+  
+    return result[0][0];
+  } catch(err) {
+    console.error(err);
+  }
 }
 
 async function deleteTask(id) {
-  const result = await mysql.query(`
-    DELETE
-    FROM tasks
-    WHERE id = ?
-  `, [id]);
-
-  return result[0][0];
+  try {
+    const result = await mysql.execute(`
+      DELETE
+      FROM tasks
+      WHERE id = ?
+    `, [id]);
+  
+    return result[0][0];
+  } catch(err) {
+    console.error(err);
+  }
 }
 
 module.exports = {
