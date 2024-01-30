d5fb66e3f9e84e5d0ac43077d5a97f824f6877fd
Kacper <xsupermarekx@gmail.com>
2024-01-30 12:01:01 +0100

    imported header and footer components

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -1,9 +1,15 @@
+import "../public/stylesheets/main.css";
+import Header from "./partials/Header.jsx";
+import Footer from "./partials/Footer.jsx";
+
 function App() {
   return (
     <>
+      <Header />
       <h1>todo app</h1>
+      <Footer />
     </>
-  )
+  );
 }
 
 export default App;
