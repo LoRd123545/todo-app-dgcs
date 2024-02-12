331e848ac99598fa19d93f6860b8cda7f89264e8
Kacper <xsupermarekx@gmail.com>
2024-02-12 13:49:05 +0100

    added html 404 response route

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -1,36 +1,42 @@
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import "../public/stylesheets/main.css";
 
 import Root from "./routes/Root";
 import Faq from "./routes/Faq";
+import PageNotFound from "./routes/PageNotFound.jsx";
 
 //Layouts
 import RootLayout from "./layouts/RootLayout.jsx";
 import io from "socket.io-client";
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
 const socket = io.connect("http://localhost:3000");
 
-
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<RootLayout />} >
+    <Route path="/" element={<RootLayout />}>
       <Route index element={<Root />} />
-      <Route path="faq" element={<Faq/ >} />
+      <Route path="faq" element={<Faq />} />
+      <Route path="*" element={<PageNotFound />} />
     </Route>
   )
-  
 );
-
 
 function App() {
   // for tests
   useEffect(() => {
-    socket.on('error', err => {
+    socket.on("error", (err) => {
       console.error(err);
-    })
-    socket.on('task-expired', data => {
-      console.log(`${data.username}'s task with id = ${data.taskID} has just expired!`);
+    });
+    socket.on("task-expired", (data) => {
+      console.log(
+        `${data.username}'s task with id = ${data.taskID} has just expired!`
+      );
     });
   }, [socket]);
 
