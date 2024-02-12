2e1525c6c64feeb9bccc43ce319c4bf03b7e606a
Kacper <xsupermarekx@gmail.com>
2024-02-12 13:58:10 +0100

    slightly changed page display

--- frontend/frontend/src/routes/PageNotFound.jsx
+++ frontend/frontend/src/routes/PageNotFound.jsx
@@ -9,19 +9,21 @@ function PageNotFound() {
           backgroundImage: `url(../public/images/PageNotFoundBackground.png)`,
         }}
       >
-        <h1 className="heading heading--big text--white text--shadow">404</h1>
-        <h3 className="heading heading--medium text--white text--shadow">
-          Ups!
-        </h3>
-        <p className="text--white text--shadow panel__paragraph paragraph--big panel__paragraph--margin">
-          Strona wydostała się z internetowej rzeczywistości i udała się na
-          wakacje do krainy fantazji. Nie martw się, wróci, jak tylko skończy
-          zwiedzać.
-        </p>
-        <div className="button__panel">
-          <Link to="/" className="button--not-found">
-            Chcę wrócić!
-          </Link>
+        <div className="container__panel--blue">
+          <h1 className="heading heading--big text--white text--shadow">404</h1>
+          <h3 className="heading heading--medium text--white text--shadow">
+            Ups!
+          </h3>
+          <p className="text--white text--shadow panel__paragraph paragraph--big panel__paragraph--margin">
+            Strona wydostała się z internetowej rzeczywistości i udała się na
+            wakacje do krainy fantazji. Nie martw się, wróci, jak tylko skończy
+            zwiedzać.
+          </p>
+          <div className="button__panel">
+            <Link to="/" className="button--not-found">
+              Chcę wrócić!
+            </Link>
+          </div>
         </div>
       </div>
     </div>
