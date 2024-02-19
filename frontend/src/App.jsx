d8b7235b088136fcba248dea39f605e5bdab2eae
LoRd123545 <kamilabbasi694@gmail.com>
2024-02-19 16:31:52 +0100

    removed second frontend folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ import RootLayout from "./layouts/RootLayout.jsx";
 import io from "socket.io-client";
 import { useEffect } from "react";
 
-const socket = io.connect("http://localhost:3000");
+const socket = io("http://localhost:3000");
 
 const router = createBrowserRouter(
   createRoutesFromElements(
