ca90aeaf824bde01243522c7f5339ca9be1de747
Kacper <xsupermarekx@gmail.com>
2024-02-24 19:05:19 +0100

    added Tasks and Tasks/add routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,8 +8,10 @@ import "../public/stylesheets/main.css";
 
 import Root from "./routes/Root";
 import Faq from "./routes/Faq";
-import About from "./routes/About"
+import About from "./routes/About";
 import PageNotFound from "./routes/PageNotFound.jsx";
+import TaskIndex from "./routes/TaskIndex.jsx";
+import TaskAdd from "./routes/TaskAdd.jsx";
 
 //Layouts
 import RootLayout from "./layouts/RootLayout.jsx";
@@ -24,6 +26,8 @@ const router = createBrowserRouter(
       <Route index element={<Root />} />
       <Route path="faq" element={<Faq />} />
       <Route path="about" element={<About />} />
+      <Route path="tasks" element={<TaskIndex />} />
+      <Route path="tasks/add" element={<TaskAdd />} />
       <Route path="*" element={<PageNotFound />} />
     </Route>
   )
