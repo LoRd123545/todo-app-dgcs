7057429588f82f873feb3f55f33b5c65a617c66e
Kacper <xsupermarekx@gmail.com>
2024-02-25 17:01:37 +0100

    task add page

--- frontend/src/routes/TaskAdd.jsx
+++ frontend/src/routes/TaskAdd.jsx
@@ -1,15 +1,60 @@
+import { useState } from "react";
+import Axios from "axios";
+
 function TaskAdd() {
+  const url = "http://localhost:3000/tasks/add";
+  const [data, setData] = useState({
+    name: "",
+    description: "",
+    completion_date: "",
+    status: "",
+  });
+
+  function submit(e) {
+    e.preventDefault();
+    Axios.post(
+      url,
+      {
+        name: data.name,
+        description: data.description,
+        completion_date: data.completion_date,
+        status: data.status,
+      },
+      {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json",
+        },
+      }
+    ).then((res) => {
+      console.log(res.data);
+    });
+  }
+
+  function handle(e) {
+    const newData = { ...data };
+    newData[e.target.id] = e.target.value;
+    setData(newData);
+    console.log(newData);
+  }
+
   return (
     <div className="form__panel">
       <div className="form__header">
         <h1 className="heading text--white ">Dodaj zadanie</h1>
       </div>
       <div className="form__box">
-        <form action="/tasks/add" method="post">
+        <form onSubmit={(e) => submit(e)}>
           <label>
             <div className="wrapper">
               <div className="input-data">
-                <input type="text" name="name" />
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  value={data.name}
+                  onChange={(e) => handle(e)}
+                />
                 <label>Nazwa Zadania</label>
               </div>
             </div>
@@ -22,6 +67,9 @@ function TaskAdd() {
                   cols="30"
                   rows="10"
                   maxlength="500"
+                  id="description"
+                  value={data.description}
+                  onChange={(e) => handle(e)}
                 ></textarea>
                 <label>Opis zadania</label>
               </div>
@@ -31,7 +79,14 @@ function TaskAdd() {
             <div className="wrapper">
               <label>Termin wykonania zadania</label>
               <div>
-                <input type="datetime-local" name="completion_date" step="1" />
+                <input
+                  type="datetime-local"
+                  name="completion_date"
+                  step="1"
+                  id="completion_date"
+                  value={data.completion_date}
+                  onChange={(e) => handle(e)}
+                />
               </div>
             </div>
           </label>
@@ -39,7 +94,12 @@ function TaskAdd() {
             <div className="wrapper">
               <label>Status zadania</label>
               <div className="select">
-                <select name="status" id="status">
+                <select
+                  name="status"
+                  id="status"
+                  value={data.status}
+                  onChange={(e) => handle(e)}
+                >
                   <option value="not-started">Do zrobienia</option>
                   <option value="in-progress">W trakcie</option>
                   <option value="done">Zakończone</option>
@@ -49,7 +109,9 @@ function TaskAdd() {
             <br />
           </label>
           <div className="wrapper wrapper__button">
-            <button className="button">Dodaj</button>
+            <button type="submit" className="button">
+              Dodaj
+            </button>
           </div>
         </form>
       </div>
