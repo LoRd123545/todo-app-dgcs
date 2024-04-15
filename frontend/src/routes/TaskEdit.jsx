ed8cadf0a539735c10e2151810057df97c86767c
LoRd123545 <kamilabbasi694@gmail.com>
2024-04-15 18:24:50 +0200

    now editing tasks is possible

--- frontend/src/routes/TaskEdit.jsx
+++ frontend/src/routes/TaskEdit.jsx
@@ -1,15 +1,89 @@
+import { axiosInstance } from "../axios.js";
+import { useAuth } from "../AuthContext";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
 function TaskEdit() {
+  const [task, setTask] = useState({});
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const keycloak = useAuth();
+  const token = keycloak.token;
+
+  useEffect(() => {
+    axiosInstance
+      .get(`http://localhost:3000/tasks/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((data) => {
+        setTask(data.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+
+    const newData = { ...task };
+    newData[e.target.name] = e.target.value;
+    setTask(newData);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const updatedTask = {
+      name: task.name || e.target.name.value,
+      description: task.description || e.target.description.value,
+      dueDate: task.dueDate || e.target.dueDate.value,
+      status: task.status || e.target.status.value,
+    };
+    setTask(updatedTask);
+    axiosInstance
+      .patch(
+        `http://localhost:3000/tasks/${id}`,
+        {
+          name: updatedTask.name,
+          dueDate: updatedTask.dueDate,
+          description: updatedTask.description,
+          status: updatedTask.status,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((data) => {
+        console.log(updatedTask);
+        console.log(data.data);
+        navigate("/tasks");
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
   return (
     <div className="form__panel">
       <div className="form__header">
         <h1 className="heading text--white ">Edytuj</h1>
       </div>
       <div className="form__box">
-        <form>
+        <form onSubmit={handleSubmit}>
           <label>
             <div className="wrapper">
               <div className="input-data">
-                <input type="text" name="name" id="name" />
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  value={task.name}
+                  onChange={handleChange}
+                />
                 <label>Nazwa Zadania</label>
               </div>
             </div>
@@ -23,6 +97,8 @@ function TaskEdit() {
                   rows="10"
                   maxlength="500"
                   id="description"
+                  value={task.description}
+                  onChange={handleChange}
                 ></textarea>
                 <label>Opis zadania</label>
               </div>
@@ -34,9 +110,11 @@ function TaskEdit() {
               <div>
                 <input
                   type="datetime-local"
-                  name="completion_date"
+                  name="dueDate"
                   step="1"
-                  id="completion_date"
+                  id="dueDate"
+                  value={task.dueDate}
+                  onChange={handleChange}
                 />
               </div>
             </div>
@@ -45,7 +123,12 @@ function TaskEdit() {
             <div className="wrapper">
               <label>Status zadania</label>
               <div className="select">
-                <select name="status" id="status">
+                <select
+                  name="status"
+                  id="status"
+                  value={task.status}
+                  onChange={handleChange}
+                >
                   <option value="not-started">Do zrobienia</option>
                   <option value="in-progress">W trakcie</option>
                   <option value="done">Zakończone</option>
@@ -56,7 +139,7 @@ function TaskEdit() {
           </label>
           <div className="wrapper wrapper__button">
             <button type="submit" className="button">
-              Dodaj
+              Edytuj
             </button>
           </div>
         </form>
