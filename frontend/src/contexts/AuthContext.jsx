491e4f7cd33f48c845b6a5c9ea74dacf775e92c0
LoRd123545 <kamilabbasi694@gmail.com>
2024-04-17 16:31:11 +0200

    moved file

---
+++ frontend/src/contexts/AuthContext.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect, useContext } from "react";
+import client from "../keycloak.js";
+
+const AuthContext = React.createContext(client);
+
+localStorage.setItem("keycloakInitialized", false);
+
+export const AuthProvider = ({ children }) => {
+  const [keycloak, setKeycloak] = useState(client);
+
+  return (
+    <AuthContext.Provider value={keycloak}>{children}</AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const keycloak = useContext(AuthContext);
+
+  useEffect(() => {
+    if (localStorage.getItem("keycloakInitialized") === "true") return;
+
+    localStorage.setItem("keycloakInitialized", true);
+
+    keycloak
+      .init({
+        onLoad: "login-required",
+      })
+      .then((res) => {
+        console.log("initialized keycloak: " + keycloak.token);
+      })
+      .catch((err) => {
+        console.error("keycloak initialization error: " + err);
+      });
+  }, [keycloak]);
+
+  return keycloak;
+};
