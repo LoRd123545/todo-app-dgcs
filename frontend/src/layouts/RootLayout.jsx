bfc950aaf75c871ec9ed0e0d1f64513da16830e8
LoRd123545 <kamilabbasi694@gmail.com>
2024-04-17 17:23:21 +0200

    changed relative paths to absolute paths

--- frontend/src/layouts/RootLayout.jsx
+++ frontend/src/layouts/RootLayout.jsx
@@ -1,7 +1,7 @@
-import {Outlet, NavLink} from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
-import Header from "../partials/Header";
-import Footer from "../partials/Footer";
+import Header from "src/partials/Header";
+import Footer from "src/partials/Footer";
 
 export default function RootLayout() {
   return (
@@ -10,5 +10,5 @@ export default function RootLayout() {
       <Outlet />
       <Footer />
     </>
-  )
+  );
 }
