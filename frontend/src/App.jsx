ffc06eb919594397916aefd47d0a1ae44c06fb27
LoRd123545 <kamilabbasi694@gmail.com>
2024-04-17 17:19:03 +0200

    unimportant change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,12 @@
 // packages
-import {
-  //createBrowserRouter,
-  //createRoutesFromElements,
-  Route,
-  //RouterProvider,
-  Routes,
-  useNavigate,
-  //Router,
-} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 // React
 import React, { useEffect, useState } from "react";
 
 // others
 import { socket } from "./socket.js";
-import { AuthProvider } from "./AuthContext.jsx";
+import { AuthProvider } from "./contexts/AuthContext.jsx";
 
 // routes
 import Root from "./routes/Root";
@@ -22,11 +14,11 @@ import Logout from "./routes/Logout";
 import Faq from "./routes/Faq";
 import About from "./routes/About";
 import PageNotFound from "./routes/PageNotFound.jsx";
-import TaskIndex from "./routes/TaskIndex.jsx";
-import TaskAdd from "./routes/TaskAdd.jsx";
-import TaskEdit from "./routes/TaskEdit.jsx";
-import TaskView from "./routes/TaskView.jsx";
-import TaskDelete from "./routes/TaskDelete.jsx";
+import TaskIndex from "./routes/tasks/TaskIndex.jsx";
+import TaskAdd from "./routes/tasks/TaskAdd.jsx";
+import TaskEdit from "./routes/tasks/TaskEdit.jsx";
+import TaskView from "./routes/tasks/TaskView.jsx";
+import TaskDelete from "./routes/tasks/TaskDelete.jsx";
 
 // layouts
 import RootLayout from "./layouts/RootLayout.jsx";
@@ -34,22 +26,6 @@ import TasksLayout from "./layouts/TasksLayout.jsx";
 
 // styles
 import "../public/stylesheets/main.css";
-
-// const router = createBrowserRouter(
-//   createRoutesFromElements(
-//     <Route path="/" element={<RootLayout />}>
-//       <Route index element={<Root />} />
-//       <Route path="faq" element={<Faq />} />
-//       <Route path="about" element={<About />} />
-//       <Route path="tasks">
-//         <Route index element={TaskIndex}></Route>
-//         <Route path="add" element={TaskAdd}></Route>
-//         <Route path=":id/edit" element={TaskEdit}></Route>
-//       </Route>
-//       <Route path="*" element={<PageNotFound />} />
-//     </Route>
-//   )
-// );
 
 function App() {
   const [isConnected, setIsConnected] = useState(socket.connected);
@@ -90,7 +66,6 @@ function App() {
 
   return (
     <div className="App">
-      {/* <RouterProvider router={router} /> */}
       <AuthProvider>
         <Routes>
           <Route path="/" element={<RootLayout />}>
