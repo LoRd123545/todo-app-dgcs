fca8525210fdb391bd42d2d262dffe70f586a745
LoRd123545 <kamilabbasi694@gmail.com>
2024-04-17 17:07:20 +0200

    added default value for dueDate field which is current date + 1 day

---
+++ backend/src/models/taskSchema.js
@@ -0,0 +1,31 @@
+import { Schema, model } from 'mongoose';
+
+const taskSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  dueDate: {
+    type: Date,
+    default: (Date.now() + 86_400_000 /* one day */),
+  },
+  status: {
+    type: String,
+    default: 'not-started',
+    enum: ['not-started', 'in-progress', 'done'],
+  },
+  description: {
+    type: String,
+    default: 'describe your task'
+  },
+  favourite: {
+    type: Boolean,
+    default: false,
+  },
+  userID: {
+    type: String,
+    required: true,
+  },
+});
+
+export default model('tasks', taskSchema);
