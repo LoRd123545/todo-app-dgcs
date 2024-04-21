1191d2f46b1b421ef582477af1ed9d9f17b10134
LoRd123545 <kamilabbasi694@gmail.com>
2024-04-21 23:19:49 +0200

    added tasks contexts

---
+++ frontend/src/contexts/TasksContext.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect, useContext } from "react";
+
+const TasksContext = React.createContext([]);
+const TasksUpdateContext = React.createContext();
+
+export const TasksProvider = ({ children }) => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    console.log(tasks);
+  }, [tasks]);
+
+  return (
+    <TasksContext.Provider value={tasks}>
+      <TasksUpdateContext.Provider value={setTasks}>
+        {children}
+      </TasksUpdateContext.Provider>
+    </TasksContext.Provider>
+  );
+};
+
+export const useTasks = () => {
+  const tasks = useContext(TasksContext);
+
+  return tasks;
+};
+
+export const useTasksUpdate = () => {
+  const setTasks = useContext(TasksUpdateContext);
+
+  return setTasks;
+};
